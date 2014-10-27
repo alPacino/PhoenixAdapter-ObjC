c043819a6df95e89c391fd1ff4bd354e8e80ec92
Justin Schneck <jschneck@mac.com>
2014-10-27 15:16:01 -0400

    Initial Commit

---
+++ PhoenixAdapter-ObjC/PhoenixAdapter-ObjC.h
@@ -0,0 +1,19 @@
+//
+//  PhoenixAdapter-ObjC.h
+//  PhoenixAdapter-ObjC
+//
+//  Created by Justin Schneck on 10/27/14.
+//  Copyright (c) 2014 LiveHelpNow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PhoenixAdapter-ObjC.
+FOUNDATION_EXPORT double PhoenixAdapter_ObjCVersionNumber;
+
+//! Project version string for PhoenixAdapter-ObjC.
+FOUNDATION_EXPORT const unsigned char PhoenixAdapter_ObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhoenixAdapter_ObjC/PublicHeader.h>
+
+
